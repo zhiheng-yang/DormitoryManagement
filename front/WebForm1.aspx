4467356f780b5b96e87e6b6a312690f830f33eb9
ZachYoung <zachyoungggg@gmail.com>
2020-11-29 12:30:19 +0800

    随便测试一下写了个查询

---
+++ front/WebForm1.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="DormitoryManagement.front.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:GridView ID="GridView1" runat="server" OnSelectedIndexChanged="GridView1_SelectedIndexChanged">
+            </asp:GridView>
+        </div>
+        <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+        <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="添加" />
+    </form>
+</body>
+</html>
---
+++ front/WebForm1.aspx.cs
@@ -0,0 +1,85 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DormitoryManagement.front
+{
+
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        private MySqlDataAdapter da;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                String strConnection = "server=49.234.112.12;port=3306;user=root;password=122316;database=gongyu;Charset=utf8;Allow Zero Datetime=True;";
+                MySqlConnection conn = new MySqlConnection(strConnection);
+                //String strSql = "SELECT * FROM USER";
+                String strSql = "SELECT * FROM REPAIR_APPLICATION";
+                conn.Open();
+                da = new MySqlDataAdapter(strSql, conn);
+                MySqlCommandBuilder cb = new MySqlCommandBuilder(da);
+                bind_gridview();
+                conn.Close();
+            }
+            catch (MySqlException ex)
+            {
+                Response.Write(ex.Message.ToString());
+            }
+        }
+
+        private void bind_gridview()
+        {
+            try
+            {
+                DataSet ds = new DataSet();
+                da.Fill(ds);
+                GridView1.DataSource = ds.Tables[0].DefaultView;
+                GridView1.DataBind();
+
+            }
+            catch (MySqlException ex)
+            {
+                Response.Write(ex.Message.ToString());
+            }
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //DataSet dst = new DataSet(); da.Fill(dst); 
+                //DataRow dr = dst.Tables[0].NewRow();
+                //dr["user_id"] = 1;
+                //dr["room_id"] = Int64.Parse(this.TextBox1.Text);
+                //dr["isCompleted"] = false; 
+                //dr["time"] = DBNull; 
+                //dst.Tables[0].Rows.Add(dr); 
+                //da.Update(dst); 
+                bind_gridview();
+            }
+            catch (MySqlException ex) 
+            { 
+                Response.Write(ex.Message.ToString()); 
+            }
+
+        }
+        public MySqlConnection getConn()
+        {
+            string connetStr = "server=49.234.112.12;port=3306;user=root;password=122316;database=gongyu;Charset=utf8;";
+            MySqlConnection conn = new MySqlConnection(connetStr);
+            return conn;
+        }
+
+
+        protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
