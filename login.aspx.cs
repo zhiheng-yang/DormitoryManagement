888388704f0ca299e53057f2f5894641696096f6
ZachYoung <zachyoungggg@gmail.com>
2020-12-16 10:54:10 +0800

    登录界面完成，还差后台

---
+++ login.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DormitoryManagement
+{
+    public partial class login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e) {
+            Response.Write("<script>alert('"+username.Text+" "+password.Text+"')</script>");
+        }
+    }
+}
