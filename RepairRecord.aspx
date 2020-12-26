569d7828f3667d55be64afc436d2e96215c3162d
ZachYoung <zachyoungggg@gmail.com>
2020-12-26 12:49:02 +0800

    杨志恒全部功能完成，看进度加一个查看与完成修改

---
+++ RepairRecord.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.Master" AutoEventWireup="true" CodeBehind="RepairRecord.aspx.cs" Inherits="DormitoryManagement.WebForm18" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+        <div class="mainbox">
+        <div class="responsive">
+            <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns ="False" AllowPaging="True">
+                <Columns>
+                    <asp:BoundField HeaderText="编号" SortExpression="id" DataField="id" HeaderStyle-BackColor="Blue" >
+<HeaderStyle BackColor="Blue"></HeaderStyle>
+                    </asp:BoundField>
+                    <asp:BoundField HeaderText="维修宿舍号" DataField="room_no" HeaderStyle-BackColor="#FF6699"  >
+<HeaderStyle BackColor="#FF6699"></HeaderStyle>
+                    </asp:BoundField>
+                    <asp:BoundField HeaderText="申请时间" DataField="time" HeaderStyle-BackColor="Lime"  >
+<HeaderStyle BackColor="Lime"></HeaderStyle>
+                    </asp:BoundField>
+
+                    <asp:BoundField HeaderText="详情描述" DataField="description" HeaderStyle-BackColor="#CC00CC"  >
+<HeaderStyle BackColor="#CC00CC"></HeaderStyle>
+                    </asp:BoundField>
+                    <asp:BoundField HeaderText="批准状态" DataField="isCompleted" HeaderStyle-BackColor="#3399FF"  >
+<HeaderStyle BackColor="#3399FF"></HeaderStyle>
+                    </asp:BoundField>
+                </Columns>
+            </asp:GridView>
+            </div>
+            </div>
+</asp:Content>
---
+++ RepairRecord.aspx.cs
@@ -0,0 +1,62 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DormitoryManagement
+{
+    public partial class WebForm18 : System.Web.UI.Page
+    {
+        private MySqlDataAdapter da;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                String strConnection = "server=49.234.112.12;port=3306;user=root;password=122316;database=gongyu;Charset=utf8;Allow Zero Datetime=True;Allow User Variables=True";
+                MySqlConnection conn = new MySqlConnection(strConnection);
+                String strSql = "select repair_application.id,room_no,time,description,isCompleted from repair_application INNER JOIN room on repair_application.room_id = room.id where isCompleted=1";
+                conn.Open();
+                da = new MySqlDataAdapter(strSql, conn);
+                MySqlCommandBuilder cb = new MySqlCommandBuilder(da);
+                bind_gridview();
+                conn.Close();
+            }
+            catch (MySqlException e1)
+            {
+                Response.Write(e1.Message.ToString());
+            }
+        }
+        private void bind_gridview()
+        {
+            try
+            {
+                DataSet ds = new DataSet();
+                da.Fill(ds);
+                GridView1.DataSource = ds.Tables[0].DefaultView;
+                GridView1.DataBind();
+                SetGrantedStatus(GridView1);
+            }
+            catch (MySqlException e2)
+            {
+                Response.Write(e2.Message.ToString());
+            }
+        }
+
+        private void SetGrantedStatus(GridView gv)
+        {
+            for (int i = 0; i < gv.Rows.Count; i++)
+            {
+
+                gv.Rows[i].Cells[4].Text = "已批准,待维修";
+               
+                //Response.Write("<script>alert(' " + gv.Rows[i].Cells[6].Text + " ');</script>");
+            }
+
+        }
+    }
+
+}
